e2f3d85617463e123820887c6c76e6f562928998
edobkowski <edobkowski@gmail.com>
2018-10-08 14:00:25 +0200

    Add Room entity class

---
+++ src/main/java/com/codecool/bookingSystem/domain/Room.java
@@ -0,0 +1,36 @@
+package com.codecool.bookingSystem.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "rooms")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    private String name;
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
