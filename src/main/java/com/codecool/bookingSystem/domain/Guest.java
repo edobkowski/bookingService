39561c0ab21274cca5bc53986e72f3131d9fed8a
edobkowski <edobkowski@gmail.com>
2018-10-08 13:59:16 +0200

    Add Guest entity class

---
+++ src/main/java/com/codecool/bookingSystem/domain/Guest.java
@@ -0,0 +1,67 @@
+package com.codecool.bookingSystem.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "guests")
+public class Guest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "phone_number")
+    private long phone;
+    private String email;
+    @OneToOne(mappedBy = "address")
+    private Address address;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
