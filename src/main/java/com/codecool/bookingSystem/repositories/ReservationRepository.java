9eafa45d3565102ecbf90ca02a8dd22cee404bb7
edobkowski <edobkowski@gmail.com>
2018-10-16 20:42:04 +0200

    Add criteria for getting reservations with payed breakfasts

--- src/main/java/com/codecool/bookingSystem/repositories/ReservationRepository.java
+++ src/main/java/com/codecool/bookingSystem/repositories/ReservationRepository.java
@@ -4,4 +4,5 @@ import com.codecool.bookingSystem.domain.Reservation;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+    Iterable<Reservation> findByBreakfastIsTrue();
 }
