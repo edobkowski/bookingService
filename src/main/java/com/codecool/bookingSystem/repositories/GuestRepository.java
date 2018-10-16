ee60a1ce877c65d835d1707d7513af454f4b0980
edobkowski <edobkowski@gmail.com>
2018-10-16 20:40:13 +0200

    Add new criterias for getting guest entities

--- src/main/java/com/codecool/bookingSystem/repositories/GuestRepository.java
+++ src/main/java/com/codecool/bookingSystem/repositories/GuestRepository.java
@@ -4,4 +4,8 @@ import com.codecool.bookingSystem.domain.Guest;
 import org.springframework.data.repository.CrudRepository;
 
 public interface GuestRepository extends CrudRepository<Guest, Long> {
+    Guest getByEmailIgnoreCase(String email);
+    Iterable<Guest> findByCityIgnoreCase(String city);
+    Iterable<Guest> findByCountryIgnoreCase(String country);
+    Iterable<Guest> findByFirstNameIgnoreCaseAndLastNameIgnoreCase(String firstName, String lastName);
 }
