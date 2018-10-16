f558a1bc635cd9ac121b9e0924190cfc1ea8a3c4
edobkowski <edobkowski@gmail.com>
2018-10-16 20:44:58 +0200

    Implement createGuest() and getters by different criterias methods

--- src/main/java/com/codecool/bookingSystem/services/GuestService.java
+++ src/main/java/com/codecool/bookingSystem/services/GuestService.java
@@ -2,6 +2,7 @@ package com.codecool.bookingSystem.services;
 
 import com.codecool.bookingSystem.domain.Address;
 import com.codecool.bookingSystem.domain.Guest;
+import com.codecool.bookingSystem.repositories.AddressRepository;
 import com.codecool.bookingSystem.repositories.GuestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,13 +17,25 @@ public class GuestService {
         this.guestRepository = guestRepository;
     }
 
-    public Guest createGuest(String name, String lastname, String phone, String email, Address address) {
-        guestRepository.save(new Guest(name, lastname, phone, email, address));
-        return null;
+    public Guest createGuest(String name, String lastname, String phone, String email, String address, String city,
+                             String country) {
+        return guestRepository.save(new Guest(name, lastname, phone, email, address, city, country));
     }
 
     public Iterable<Guest> getAll() {
         return guestRepository.findAll();
+    }
+
+    public Iterable<Guest> getByFullName(String firstName, String lastName) {
+        return guestRepository.findByFirstNameIgnoreCaseAndLastNameIgnoreCase(firstName, lastName);
+    }
+
+    public Iterable<Guest> getByCity(String city) {
+        return guestRepository.findByCityIgnoreCase(city);
+    }
+
+    public Iterable<Guest> getByCountry(String country) {
+        return guestRepository.findByCountryIgnoreCase(country);
     }
 
     public long total() {
